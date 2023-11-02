edc0345fef91a9542cde0acf3b0c25c1c7a51cfb
YamaokaShinsaku <YamaokaShinsaku@users.noreply.github.com>
2023-11-02 21:46:41 +0900

    Wave処理の作成
    ウェーブが切り替わると、ウェーブ回数分のエネミーを生成する

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// 各ウェーブごとにエネミーの数などを設定する
+/// </summary>
+public class WaveSpawner : MonoBehaviour
+{
+    // スポーンさせるEnemy
+    public GameObject enemyPrefab;
+    public Transform spawnPoint;
+    // ウェーブ間の時間
+    public float timeBetweenWaves = 5.0f;
+    public Text waceCountDownText;
+
+    private float countDown = 2.0f;
+
+    // Wave番号
+    private int waveNumber = 0;
+
+    void Update()
+    {
+        if(countDown <= 0.0f)
+        {
+            // ウェーブを生成する
+            StartCoroutine(CreateWave());
+            countDown = timeBetweenWaves;
+        }
+
+        countDown -= Time.deltaTime;
+        // (小数点以下切り捨て)
+        waceCountDownText.text = Mathf.Round(countDown).ToString();
+    }
+
+    /// <summary>
+    /// ウェーブを生成する
+    /// </summary>
+    IEnumerator CreateWave()
+    {
+        waveNumber++;
+        Debug.Log("Create Wave " + waveNumber);
+        for(int i = 0; i < waveNumber; i++) 
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    /// <summary>
+    /// Enemyを生成する
+    /// </summary>
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
