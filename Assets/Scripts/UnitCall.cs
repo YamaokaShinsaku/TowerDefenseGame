08a8e3b8f8627c7a333feb3c45f606fe08e7a7f3
YamaokaShinsaku <YamaokaShinsaku@users.noreply.github.com>
2023-11-07 00:25:30 +0900

    UnitCall.csの仕様の修正
    ユニットデータを個別ではなく配列で扱うように修正

--- Assets/Scripts/UnitCall.cs
+++ Assets/Scripts/UnitCall.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// 配置するユニットの取得を行う
@@ -6,8 +7,20 @@ using UnityEngine;
 public class UnitCall : MonoBehaviour
 {
     // 各プレイヤーユニットのデータを設定
-    public PlayerUnitData unit1Data;
-    public PlayerUnitData unit2Data;
+    //public PlayerUnitData unit1Data;
+    //public PlayerUnitData unit2Data;
+    public PlayerUnitData[] unitDatas;
+    // 各プレイヤーユニットの使用コストを表示するUI
+    public Text[] unitUseCostText;
+
+    private void Start()
+    {
+        // 使用コストを表示
+        for (int i = 0; i < unitDatas.Length; i++)
+        {
+            unitUseCostText[i].text = unitDatas[i].cost.ToString();
+        }
+    }
 
     /// <summary>
     /// PlayerUnit1を取得
@@ -15,7 +28,8 @@ public class UnitCall : MonoBehaviour
     public void SelectPlayerUnit1()
     {
         Debug.Log("Get PlayerUnit_1");
-        BuildManager.instance.SelectPlayerUnitToBuild(unit1Data);
+        //BuildManager.instance.SelectPlayerUnitToBuild(unit1Data);
+        BuildManager.instance.SelectPlayerUnitToBuild(unitDatas[0]);
     }
 
     /// <summary>
@@ -24,6 +38,7 @@ public class UnitCall : MonoBehaviour
     public void SelectPlayerUnit2()
     {
         Debug.Log("Get PlayerUnit_2");
-        BuildManager.instance.SelectPlayerUnitToBuild(unit2Data);
+        //BuildManager.instance.SelectPlayerUnitToBuild(unit2Data);
+        BuildManager.instance.SelectPlayerUnitToBuild(unitDatas[1]);
     }
 }
