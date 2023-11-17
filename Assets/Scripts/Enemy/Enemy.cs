c852b57dd7ceb22215f4890cf3e53434e447bdf8
YamaokaShinsaku <YamaokaShinsaku@users.noreply.github.com>
2023-11-17 17:13:27 +0900

    Enemy.csをエネミーのデータ管理と移動処理の2つに分けた

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -11,59 +11,7 @@ public class Enemy : MonoBehaviour
     public float speed = 10.0f;
     // 死亡時のエフェクト
     public GameObject dieEffect;
-    // 移動先のオブジェクト
-    private Transform target;
-    // 現在の移動先オブジェクトの番号
-    private int movePointIndex = 0;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        target = MovePoints.movePoints[0];
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // 移動先ベクトルを計算し、移動させる
-        Vector3 direction = target.position - this.transform.position;
-        this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
-
-        // targetに十分に近づいたとき
-        if(Vector3.Distance(this.transform.position, target.position) <= 0.2f)
-        {
-            GetNextMovePoint();
-        }
-    }
-
-    /// <summary>
-    /// 次のMovePointを取得
-    /// </summary>
-    void GetNextMovePoint()
-    {
-        // 最後のMovePointに到達したとき
-        if(movePointIndex >= MovePoints.movePoints.Length - 1)
-        {
-            EndPoint();
-            return;
-        }
-
-        movePointIndex++;
-        // targetに次のMovePointを設定
-        target = MovePoints.movePoints[movePointIndex];
-    }
-
-    /// <summary>
-    /// 終点に到達したときの処理
-    /// </summary>
-    void EndPoint()
-    {
-        // ライフを減算
-        PlayerStats.life--;
-        // 自身を削除
-        Destroy(this.gameObject);
-    }
 
     /// <summary>
     /// ダメージを受ける
--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -3,17 +3,62 @@ using UnityEngine;
 /// <summary>
 /// 敵の移動管理
 /// </summary>
+[RequireComponent(typeof(Enemy))]
 public class EnemyMovement : MonoBehaviour
 {
+    // 移動先のオブジェクト
+    private Transform target;
+    // 現在の移動先オブジェクトの番号
+    private int movePointIndex = 0;
+
+    private Enemy enemy;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemy = GetComponent<Enemy>();
+        target = MovePoints.movePoints[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 移動先ベクトルを計算し、移動させる
+        Vector3 direction = target.position - this.transform.position;
+        this.transform.Translate(direction.normalized * enemy.speed * Time.deltaTime, Space.World);
+
+        // targetに十分に近づいたとき
+        if (Vector3.Distance(this.transform.position, target.position) <= 0.2f)
+        {
+            GetNextMovePoint();
+        }
+    }
+
+    /// <summary>
+    /// 次のMovePointを取得
+    /// </summary>
+    void GetNextMovePoint()
+    {
+        // 最後のMovePointに到達したとき
+        if (movePointIndex >= MovePoints.movePoints.Length - 1)
+        {
+            EndPoint();
+            return;
+        }
+
+        movePointIndex++;
+        // targetに次のMovePointを設定
+        target = MovePoints.movePoints[movePointIndex];
+    }
+
+    /// <summary>
+    /// 終点に到達したときの処理
+    /// </summary>
+    void EndPoint()
+    {
+        // ライフを減算
+        PlayerStats.life--;
+        // 自身を削除
+        Destroy(this.gameObject);
     }
 }
